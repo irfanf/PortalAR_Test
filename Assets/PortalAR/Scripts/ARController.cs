e4459a5fbc56be50ddd3db2cbd609c3ed49ca200
ｲﾙﾌｧﾝ ﾌｧｰﾐ ﾗﾏﾀﾞﾝ <irfan_fahmi@cyberagent.co.jp>
2018-09-07 17:49:15 +0900

    First commit

---
+++ Assets/PortalAR/Scripts/ARController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ARController : MonoBehaviour
+{
+
+    //planes that ARCore detected in the current frames
+    private List<TrackedPlane> newTrackedPlanes = new List<TrackedPlane>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check ARCore session status
+        if (Session.Status != SessionStatus.Tracking) return;
+
+        //fill newTrackedPlanes with new planes
+        Session.GetTrackables<TrackedPlane>(newTrackedPlanes, TrackableQueryFilter.New);
+    }
+}
